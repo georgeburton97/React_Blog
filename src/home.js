6749af3f6db9c7c8a8ea156d4e11bd4ae2b4dd9f
GEORGE BURTON <georgeburton@live.co.uk>
2021-02-24 22:02:46 +0000

    Added blog list prop

--- src/home.js
+++ src/home.js
@@ -1,25 +1,19 @@
-import {useState} from 'react';
+
+import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
-    const [blogs, setBlogs] = useState([
-        {title: "My new website", body: "lorem ipsum...", author: "mario", id: 1},
-        {title: "Welcome", body: "lorem ipsum...", author: "peach", id: 2},
-        {title: "Web dev tips", body: "lorem ipsum...", author: "toad", id: 3},
-    ]);
-
-
-    return ( 
-        <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
- 
-        </div>
-     );
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" />
+    </div>
+  );
 }
  
 export default Home;
