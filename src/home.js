65a77e085d2d8feeec8b4a9c75a4386162f94697
GEORGE BURTON <georgeburton@live.co.uk>
2021-02-26 19:16:44 +0000

    added delete function as prop

--- src/home.js
+++ src/home.js
@@ -4,14 +4,22 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'My new website', body: 'lorem ipsum...', author: 'george', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'george', id: 3 }
+  ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => {
+      return blog.id !== id;
+    });
+    setBlogs(newBlogs);
+  }
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'george')} title="George's Blogs" /> */}
     </div>
   );
 }
