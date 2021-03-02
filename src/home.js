fa9443e64f854a0c2d77d0e66904221d3bc3986a
GEORGE BURTON <georgeburton@live.co.uk>
2021-03-02 18:04:41 +0000

    Added fetch to blog data

--- src/home.js
+++ src/home.js
@@ -1,25 +1,34 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'george', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'george', id: 3 }
-  ]);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => {
-      return blog.id !== id;
-    });
-    setBlogs(newBlogs);
-  }
+ 
+
+
+
+  useEffect(() => {
+    console.log('use effect ran');
+    fetch('http://localhost:8000/blogs')
+      .then(res => res.json())
+      .then((data) => {
+        setBlogs(data);
+        setIsPending(false);
+      })
+ 
+    
+  }, [] );
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'george')} title="George's Blogs" /> */}
+
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+
+      
     </div>
   );
 }
